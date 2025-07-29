43ab81f360d34cc5a5bc4f53be8581af5f854d79
OleksandrObydalo <oleksandr.obydalo@nure.ua>
2025-07-29 16:05:09 +0300

    Create database and get connection

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,17 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
+        String url = "jdbc:h2:file:./test";
+        String username = "sa";
+        String password = "password";
+        try(Connection connection = DriverManager.getConnection(url, username, password);) {
+            System.out.println("Is connection valid? " + connection.isValid(1000));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
